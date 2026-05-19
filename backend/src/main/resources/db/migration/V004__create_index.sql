c73a474d5e2933bca5e6242fffa5dacfb521918c
Andrey F. Mindubaev <andrey.mindubaev@apnet.ru>
2026-05-19 23:40:23 +0300

    added sequence values to ids

--- backend/src/main/resources/db/migration/V004__create_index.sql
+++ backend/src/main/resources/db/migration/V004__create_index.sql
@@ -1,2 +1,12 @@
 CREATE INDEX order_product_order_id_idx ON order_product(order_id);
 CREATE INDEX orders_status_date_idx ON orders(status, date_created);
+
+SELECT setval(
+  pg_get_serial_sequence('orders', 'id'),
+  (SELECT MAX(id) FROM orders)
+);
+
+SELECT setval(
+  pg_get_serial_sequence('product', 'id'),
+  (SELECT MAX(id) FROM product)
+);
